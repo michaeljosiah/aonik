bca3ba3f0ae67c67faa50e943ababfd8304a8f8c
Michael Josiah <mjosiah@nextgensystems.co.uk>
2026-01-14 23:49:38 +0000

    Add generated docs-site assets

---
+++ docs-site/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\src\\css\\custom.css"),
+];
