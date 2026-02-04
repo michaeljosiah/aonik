ed3a827342b1ff9995ef646a0ec98d8c8aa1cfa1
Michael Josiah <25878095+michaeljosiah@users.noreply.github.com>
2026-02-04 05:22:47 +0000

    Fix Payabo preloader overlay

--- Payabo/src/components/common/Preloader.tsx
+++ Payabo/src/components/common/Preloader.tsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 export const Preloader = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setIsVisible(false);
+    }, 500);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div id="loading">
       <div id="loading-center">
