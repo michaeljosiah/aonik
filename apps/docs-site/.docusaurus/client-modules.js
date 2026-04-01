427b6a261e229eb9ac53850db68f00ef1228cfd6
Michael Josiah <mjosiah@nextgensystems.co.uk>
2026-04-01 06:51:36 +0100

    chore(repo): ignore local agent files and remove docs build artifacts

--- apps/docs-site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\mjosi\\source\\repos\\aonik\\docs-site\\src\\css\\custom.css"),
-];
